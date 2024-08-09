35458062cbd51d8964749732aaee5568d05553ca
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-08-09 22:37:08 +0530

    Add files via upload
    Faulty calculater with the help of youtube

---
+++ Faulty calculater/script.js
@@ -0,0 +1,21 @@
+let random = Math.random(
+)
+let a = prompt("enter first number")
+let b = prompt("enter second number")
+let c = prompt("enter third number")
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "*",
+
+}
+c =obj
+[c]
+if (random > 0.1) {
+alert("the result is $")
+}
+else {
+c = obj[c]
+}
